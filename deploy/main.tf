2d7be5c3ae834c4e37cdd0c27e6bac701330b1e4
Uma0310 <umamudkhede7@gmail.com>
2024-02-23 17:23:37 +0530

    commit

--- deploy/main.tf
+++ deploy/main.tf
@@ -1,9 +1,9 @@
-terraform {
-    cloud {
-        organization = "group4dbda"
+# terraform {
+#     cloud {
+#         organization = "group4dbda"
 
-        workspaces {
-            name = "group4dbda"
-        }
-    }
-}
+#         workspaces {
+#             name = "group4dbda"
+#         }
+#     }
+# }
